d00e8de02537e1b31f358e01a1853538948bec43
Gabriel Croitoru <gabriel.croitoru@zitec.com>
2016-05-01 10:39:29 +0300

    implemented multivariable liniar descent

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,13 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
+    I = X * theta - y;
+    delta = (I' * X)' / m;
+    theta = theta - (alpha * delta);
 
 
 
